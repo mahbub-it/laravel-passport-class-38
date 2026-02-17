27f8dd2bb8eff33c472f89989d2ad7fc2cd9870a
mahbub-it <bdsmrahman@gmail.com>
2026-02-18 04:05:46 +0600

    Class 38 end

---
+++ routes/api.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', function (Request $request) {
+
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:8',
+    ]);
+
+    $user = new User([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+
+    if ($user->save()) {
+        return response()->json([
+            'message' => 'User registration successfully',
+            'user' => $user,
+        ]);
+    } else {
+        return response()->json([
+            'error' => 'User registration failed',
+        ]);
+    }
+});
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+
+        $user = auth()->user();
+
+        $token = $user->createToken('token');
+
+        return $token;
+    }
+});
+
+Route::get('/profile', function () {
+
+    return 'My Profile';
+
+})->middleware('auth:api');
